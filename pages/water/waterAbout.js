efd49921202ad8525a9a5f5e59a34ed3273a94c3
ilya smagin <ilya-z1999@ya.ru>
2023-01-06 03:13:32 +0300

    default plant prop

--- pages/water/waterAbout.js
+++ pages/water/waterAbout.js
@@ -10,7 +10,14 @@ import { useRef, useState, useEffect } from "react";
 import BackIcon from "../components/icons/back";
 
 export default function WaterAbout({
-  plant,
+  plant = {
+    id: 1,
+    name: "Hedgehog Aloe",
+    description: "Plant",
+    wateringRegularity: 96,
+    lastWateringTime: "2022-11-24T20:29:30.851719+00:00",
+    wateringVolume: 200,
+  },
   handleWatering,
   cancelWatering,
   index,
