803fd0136df3033707d2cb89fecade2db2432260
ilya smagin <ilya-z1999@ya.ru>
2023-01-15 23:51:06 +0300

    added ontap & hold button animations

--- pages/index.js
+++ pages/index.js
@@ -208,12 +208,13 @@ export default function Home() {
           <div className="text-md font-bold leading-none mb-2 pr-6">
             {plant.name}
           </div>
-          <button
+          <motion.button
+            whileTap={{ scale: 0.8 }}
             onClick={() => addPlantToUserArray(plant.id, username)}
             className="rounded-full bg-white/30 text-white w-8 h-8 flex justify-center items-center text-2xl pb-1"
           >
             +
-          </button>
+          </motion.button>
         </motion.li>
       );
     });
--- pages/water/waterItem.js
+++ pages/water/waterItem.js
@@ -24,6 +24,7 @@ export default function WaterItem({
 }) {
   const [counter, setCounter] = useState(100);
   const intervalRef = useRef(null);
+  const [touch, setTouch] = useState(false);
 
   const startCounter = () => {
     if (intervalRef.current) return;
@@ -48,7 +49,8 @@ export default function WaterItem({
     return stopCounter();
   }, []);
   return (
-    <div
+    <motion.div
+      animate={{ scale: touch ? 0.97 : 1 }}
       className={
         "cursor-pointer pr-4  pl-0 py-4  min-h-[6rem] rounded-xl w-full text-white overflow-hidden h-full flex flex-row justify-between items-center " +
         (calculateNextWatering(
@@ -59,31 +61,37 @@ export default function WaterItem({
           : "bg-[#e5e9ea]")
       }
     >
-      <div className=" flex-none relative h-full w-1/4">
-        <Image
-          className="object-contain"
-          src={`/p${plant.id}.png`}
-          alt={"Drawing of " + plant.name}
-          fill
-          sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
-        />
-      </div>
-
       <div
-        className="mb-2 flex-1 self-start"
+        className="flex flex-row justify-between items-center w-full h-full"
+        onMouseDown={() => setTouch(true)}
+        onPointerUp={() => setTouch(false)}
+        onMouseLeave={() => setTouch(false)}
         onClick={() => setWhichIsOpen(index)}
       >
-        <div className="text-lg font-bold leading-none">{plant.name}</div>
-        <div className=" mt-3 text-slate-500 flex flex-row flex-nowrap">
-          <div className=" flex justify-center items-baseline">
-            <VolumeIcon className="w-6 h-6" />
+        <div className=" flex-none relative h-full w-1/4">
+          <Image
+            className="object-contain"
+            src={`/p${plant.id}.png`}
+            alt={"Drawing of " + plant.name}
+            fill
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+          />
+        </div>
+
+        <div className="mb-2 flex-1 self-start">
+          <div className="text-lg font-bold leading-none">{plant.name}</div>
+          <div className=" mt-3 text-slate-500 flex flex-row flex-nowrap">
+            <div className="flex justify-center items-baseline">
+              <VolumeIcon className="w-6 h-6" />
+            </div>
+            <div className=" font-normal">~ {plant.wateringVolume} ml</div>
           </div>
-          <div className=" font-normal">~ {plant.wateringVolume} ml</div>
         </div>
       </div>
-      <button //TODO touch handle pass to the button
+      <motion.button //TODO touch handle pass to the button
+        whileTap={{ scale: 0.8 }}
         style={{
           backgroundColor:
             calculateNextWatering(
@@ -95,11 +103,31 @@ export default function WaterItem({
                 : ""
               : "",
         }}
-        className="flex-none flex justify-center items-center rounded-full bg-white/30 text-white w-12 h-12 "
-        onClick={(e) => handleWatering(e, index)}
-        onMouseDown={startCounter}
-        onMouseUp={(e) => stopCounter(e, index)}
-        onMouseLeave={(e) => stopCounter(e, index)}
+        className="flex-none flex justify-center items-center rounded-full bg-white/30 text-white w-12 h-12 ml-4"
+        onClick={(e) => {
+          e.stopPropagation();
+          handleWatering(e, index);
+        }}
+        onMouseDown={(e) => {
+          e.stopPropagation();
+          startCounter(e);
+        }}
+        onMouseUp={(e) => {
+          e.stopPropagation();
+          stopCounter(e, index);
+        }}
+        onMouseLeave={(e) => {
+          e.stopPropagation();
+          stopCounter(e, index);
+        }}
+        onTouchStart={(e) => {
+          e.stopPropagation();
+          startCounter(e, index);
+        }}
+        onTouchEnd={(e) => {
+          e.stopPropagation();
+          stopCounter(e, index);
+        }}
       >
         {calculateNextWatering(
           plant.lastWateringTime,
@@ -109,7 +137,7 @@ export default function WaterItem({
         ) : (
           <DropIcon className="h-7 w-7" />
         )}
-      </button>
-    </div>
+      </motion.button>
+    </motion.div>
   );
 }
